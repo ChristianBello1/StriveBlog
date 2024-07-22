f04b18c1ec0e61289a8b0297748b15511e9ded2c
Christianbello1 <lulabchannel2@gmail.com>
2024-07-22 17:41:58 +0200

    iconcina

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { getUserData } from "../services/api"; // Adjust the path as necessary
 import "./Navbar.css";
+import strivelogo from '../assets/strivelogo.png';
 
 export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -48,7 +49,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div id="navbar" className="container-fluid">
         <Link to="/" className="navbar-brand">
-          Blog App
+          <img className="logo" src={strivelogo} alt="Strive Logo" />
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
